37365eaedf6e25ab609bee9506617f7f0d5b3dec
Carlos <142463805+CarlosMII@users.noreply.github.com>
2023-10-23 12:52:52 -0700

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,10 @@
+#set language to English
+Sys.setlocale("LC_TIME", "English")
+
+#format time vector
+date_converted <- as.Date(data[, 1], format = "%d/%m/%Y")
+time_converted <- strptime(data[, 2], format = "%H:%M:%S")
+time_axis <- as.POSIXct(paste(date_converted, data[, 2]))
+
+##plot global active power against time vector
+plot(time_axis, data[, 3], xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
